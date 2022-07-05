054d5281d9fd618eea686d5902db444864ab888a
igor ricci <assis.ricci@gmail.com>
2022-07-05 18:47:35 -0300

    add flipping animation

--- app.js
+++ app.js
@@ -143,12 +143,15 @@ const flipTiles = () => {
   rowTiles.forEach((tile, index) => {
     const dataLetter = tile.getAttribute('data')
 
-    if (dataLetter == wordle[index]) {
-      tile.classList.add('green-overlay')
-    } else if (wordle.includes(dataLetter)) {
-      tile.classList.add('yellow-overlay')
-    } else {
-      tile.classList.add('grey-overlay')
-    }
+    setTimeout(() => {
+      tile.classList.add('flip')
+      if (dataLetter == wordle[index]) {
+        tile.classList.add('green-overlay')
+      } else if (wordle.includes(dataLetter)) {
+        tile.classList.add('yellow-overlay')
+      } else {
+        tile.classList.add('grey-overlay')
+      }
+    }, 250 * index)
   })
 }
