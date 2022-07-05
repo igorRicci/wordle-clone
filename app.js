49ef9f406c4c13f8877fbb7cde25ee30a1a0fab8
igor ricci <assis.ricci@gmail.com>
2022-07-05 12:51:21 -0300

    create basic keyboard with JS

---
+++ app.js
@@ -0,0 +1,39 @@
+const tileDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.keyboard-container')
+
+const keys = [
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'ENTER',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  '«',
+]
+
+keys.forEach((key) => {
+  const buttonElement = document.createElement('button')
+  buttonElement.textContent = key
+  keyboard.append(buttonElement)
+})
