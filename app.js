88f482da557b6fda367e420526507a881b7350ee
igor ricci <assis.ricci@gmail.com>
2022-07-05 14:25:52 -0300

    can add letter to one tile

--- app.js
+++ app.js
@@ -41,6 +41,9 @@ const guessRows = [
   ['', '', '', '', '']
 ]
 
+let currentRow = 0
+let currentTile = 0
+
 guessRows.forEach((guessRow, guessRowIndex) => {
   const rowElement = document.createElement('div')
   rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
@@ -53,14 +56,20 @@ guessRows.forEach((guessRow, guessRowIndex) => {
   tileDisplay.append(rowElement)
 })
 
-const handleClick = () => {
-  console.log('clicked');
-}
-
 keys.forEach((key) => {
   const buttonElement = document.createElement('button')
   buttonElement.textContent = key
   buttonElement.setAttribute('id', key)
-  buttonElement.addEventListener('click', handleClick)
+  buttonElement.addEventListener('click', () => handleClick(key))
   keyboard.append(buttonElement)
 })
+
+const handleClick = (key) => {
+  console.log('clicked', key);
+  addLetter(key)
+}
+
+const addLetter = (letter) => {
+  const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+  tile.textContent = letter
+}
